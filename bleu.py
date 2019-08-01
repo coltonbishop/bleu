bdb653187db88011334f122b2dd0d7fcf958ce15
Colton-Bishop <cmbishop@princeton.edu>
2019-08-01 16:49:12 -0400

    Tokenization of Sentences, Calculation of Score

--- bleu.py
+++ bleu.py
@@ -1,5 +1,14 @@
 from nltk.translate.bleu_score import sentence_bleu
+from googletrans import Translator
+import nltk
 
+# Takes original sentence and guess
+base = "It is so lovely to meet you!"
+base_tok = nltk.word_tokenize(base)
+guess = "Es tan lindo conocerte!"
+guess_tok = nltk.word_tokenize(guess)
+
+translator = Translator()
 
 reference = [['this', 'is', 'a', 'test'], ['this', 'is' 'test']]
 candidate = ['this', 'is', 'a', 'test']
