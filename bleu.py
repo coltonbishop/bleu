5274bef0ebc7dd2dcfda82a1d25a7bde6b5417ec
Colton-Bishop <cmbishop@princeton.edu>
2019-08-02 13:25:06 -0400

    Encoding

--- bleu.py
+++ bleu.py
@@ -1,18 +1,29 @@
+
 from nltk.translate.bleu_score import sentence_bleu
 from googletrans import Translator
 import nltk
 
+reference = []
+
 # Takes original sentence and guess
-base = "It is so lovely to meet you!"
-base_tok = nltk.word_tokenize(base)
-guess = "Es tan lindo conocerte!"
-guess_tok = nltk.word_tokenize(guess)
+base = "I am 11 years old."
+
+
+guess = u"Tengo 11 años."
+candidate = nltk.word_tokenize(guess)
 
 translator = Translator()
+true = translator.translate(base, src='en', dest='es')
+reference.append(nltk.word_tokenize(true.text))
 
-reference = [['this', 'is', 'a', 'test'], ['this', 'is' 'test']]
-candidate = ['this', 'is', 'a', 'test']
-
+# sentence_bleu(reference, candidate)
 score = sentence_bleu(reference, candidate)
+
+print(reference)
+print(candidate)
+print("Base: {}".format(base))
+print("Guess: {}".format(guess))
+print("Reference: {}".format(true.text.encode('utf-8')))
+
 
 print(score)
